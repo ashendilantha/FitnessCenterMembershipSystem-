813c2a716e3f623777fbf570639e348e6bf00bd7
ashendilantha <ashendilantha99@gmail.com>
2025-05-14 20:21:10 +0530

    update all files

--- src/main/java/lk/sliit/fitnesscenter/fitnesscentermembershipsystem/model/CashPayment.java
+++ src/main/java/lk/sliit/fitnesscenter/fitnesscentermembershipsystem/model/CashPayment.java
@@ -1,39 +1,3 @@
-//package lk.sliit.fitnesscenter.fitnesscentermembershipsystem.model;
-//
-//import java.time.LocalDateTime;
-//
-//public class CashPayment extends Payment {
-//    private String receiptNumber;
-//
-//    public CashPayment(int paymentId, int memberId, String planId, String classId, double amount, LocalDateTime paymentDate, String description, String receiptNumber) {
-//        super();
-//        super.setPaymentMethod("Cash");
-//    }
-//
-//    public CashPayment(int paymentId, int memberId, String planId, String classId,
-//                       double amount, LocalDateTime paymentDate, String description,
-//                       String receiptNumber) {
-//        super(paymentId, memberId, planId, classId, amount, paymentDate, description, "Cash");
-//        this.receiptNumber = receiptNumber;
-//    }
-//
-//    // Getters and Setters
-//    public String getReceiptNumber() {
-//        return receiptNumber;
-//    }
-//
-//    public void setReceiptNumber(String receiptNumber) {
-//        this.receiptNumber = receiptNumber;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return super.toString() + "," + receiptNumber;
-//    }
-//}
-
-
-
 package lk.sliit.fitnesscenter.fitnesscentermembershipsystem.model;
 
 import java.time.LocalDateTime;
